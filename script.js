8ccd1cf648a927503cfa8d0bf59b0f1b166c5826
JoeOwino <joeoowino@gmail.com>
2025-06-09 16:27:21 +0300

    feat: add functionality to display username or guest in the profile section

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ let loadedDoc = window.location.pathname.split('/').pop()
 if (loadedDoc === 'login.html') {
     let loginForm = document.getElementById('loginForm');
     loginForm.addEventListener('submit', handleLoginSubmit);
+} else {
+    getUserName();
+}
+
+function getUserName() {
+    let usee_name = document.getElementById('usee_name');
+    let token = localStorage.getItem('zone01_token');
+
+    if (token) {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        usee_name.innerText = payload.name || payload.email || 'User';
+    } else {
+        usee_name.textContent = 'Guest';
+    }
 }
 
