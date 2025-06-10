f2c1a039cf0c61328d254342e6640b258a46d1cf
JoeOwino <joeoowino@gmail.com>
2025-06-10 12:29:30 +0300

    feat: add logging for fetched data in user model

--- model.js
+++ model.js
@@ -2,6 +2,8 @@ import { schema } from "./query.js";
 
 const { data } = (await schema()) || { data: {} };
 
+console.log("Fetched data:", data);
+
 export const user = {
   id: data?.user?.id ?? null,
   login: data?.user?.login ?? "Guest",
