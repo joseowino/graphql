a6e6607cb02bbdcfe3fae9c26dc1f6a9fc0bc459
JoeOwino <Joeoowino@gmail.com>
2025-06-14 21:31:19 +0300

    reafctor: add header text

--- profile.js
+++ profile.js
@@ -1,11 +1,19 @@
 import { user } from './model.js';
 
 const login = document.getElementById('login');
+const header = document.getElementById('header'); 
 
 export function getProfile() {
     if (!user) {
         console.error('Not able to fetch data.');
         return;
+    }
+
+    if (header) {
+        if (user.campus && user.campus !== null) {
+            const campusName = user.campus.charAt(0).toUpperCase() + user.campus.slice(1);
+            header.innerText = `Zone01 ${campusName} Dashboard`;
+        } 
     }
 
     if (login) {
