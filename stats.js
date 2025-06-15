75281290c4c61f9fc64c1968f5f2627a72ecf6a5
JoeOwino <Joeoowino@gmail.com>
2025-06-15 10:23:28 +0300

    fix: update getStats function to remove user check and simplify level display

--- stats.js
+++ stats.js
@@ -1,14 +1,8 @@
-import { user } from "./model.js";
 const level = document.getElementById('level');
 
-export function getStats() {
-    if (!user) {
-        console.error('Not able to fetch data.');
-        return;
-    }
-
+export function getStats(user) {
     if (level) {
-        level.innerText = `Level: ${user.level}`;
+        level.innerText = user.level;
     }
 
 
