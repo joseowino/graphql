8e302b37ae8b4575cc4a1bf7fd469c47bca01a88
joseowin <joeoowino@gmail.com>
2025-06-15 15:47:55 +0300

    feat: add getRanks function to determine current and next user ranks

--- calc.js
+++ calc.js
@@ -3,3 +3,22 @@ export function setText(element, text) {
         element.innerText = text;
     }
 }
+
+export function getRanks(user) {
+    const userLevel = user.level;
+    const ranks = user.events.ranksDefinitions;
+
+    // Get current rank
+    const currentRank = ranks
+        .filter(r => r.level <= userLevel)
+        .sort((a, b) => b.level - a.level)[0];
+
+    // Get next rank
+    const nextRank = ranks
+        .filter(r => r.level > userLevel)
+        .sort((a, b) => a.level - b.level)[0];
+
+    return { currentRank, nextRank };
+}
+
+
